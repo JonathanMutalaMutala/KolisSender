644b21684b64ffc152ab10e6c9a5aca065d24efe
Mutala Mutala <jonamut12.live@gmail.com>
2024-09-10 19:18:36 -0400

    Add itinerary Model

---
+++ KolisSender.Core.Domain/Entities/Itinerary.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KolisSender.Core.Domain.Entities
+{
+    public class Itinerary
+    {
+        public string Id { get; set; }
+        public string DepartureAirport { get; set; } = default!; 
+        public string DestinationAirport { get; set; } = default!; 
+        public string DepartureCountry { get; set; } = default!;
+        public string DestinationCountry { get; set; } = default!; 
+        public string DepartureCity { get; set; } = default!; 
+        public string DestinationCity { get; set; } = default!;
+        public bool IsActive { get; set; } 
+        public bool IsValid { get; set; }
+
+        [Required]
+        public DateTime DepartureDate { get; set; }
+        [Required]
+        public DateTime DestinationDate { get; set; }
+
+    }
+}
